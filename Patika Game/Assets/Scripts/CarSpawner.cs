dd733c707b8948990b7eff1ad70f9586f5aad259
Emre Çınar <121261363+emrecnr@users.noreply.github.com>
2023-05-18 22:51:27 +0300

    Spawn Interval Decrease Added

--- Patika Game/Assets/Scripts/CarSpawner.cs
+++ Patika Game/Assets/Scripts/CarSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
@@ -7,31 +8,54 @@ public class CarSpawner : MonoBehaviour
     public GameObject[] cars;
     public Transform[] carsSpawnPoint;
 
-    private float spawnInterval = 2f;
+    private float spawnInterval=5;
     private Vector3 spawnRotation = new Vector3(0, 180, 0);
     private WaitForSeconds waitTime;
 
+    private float intervalDecreaseAmount = 0.5f; // spawn aralýðýnýn azalma miktari
+    public float elapsedTime;//gecen süre
     public bool gameOn = true;
+
+
 
     private void Start()
     {
+        
         waitTime = new WaitForSeconds(spawnInterval);
         StartCoroutine(SpawnCar());
+
+        elapsedTime = 0f;
     }
-    IEnumerator SpawnCar() 
+
+    private void Update()
     {
-        while(gameOn) 
+        SpawnDecrease();
+    }
+    private void SpawnDecrease()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= 15)
+        {
+            spawnInterval -= intervalDecreaseAmount;
+            elapsedTime = 0f;
+        }
+    }
+    IEnumerator SpawnCar()
+    {
+        while (gameOn)
         {
             int carIndex = Random.Range(0, cars.Length);
             int spawnIndex = Random.Range(0, carsSpawnPoint.Length);
 
             Vector3 spawnPosition = carsSpawnPoint[spawnIndex].position;
 
-            Instantiate(cars[carIndex], spawnPosition,Quaternion.identity);
-            yield return waitTime;
+            Instantiate(cars[carIndex], spawnPosition, Quaternion.identity);
+
+
+            yield return new WaitForSeconds(spawnInterval);
 
         }
-      
+
     }
-   
+
 }
